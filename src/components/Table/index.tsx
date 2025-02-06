75c814e6c99a3c14612f2540ab3861db5738439d
Stéphane Bazzé <zarakeye@users.noreply.github.com>
2025-02-06 14:43:25 +0100

    Beginning to implement handleSearch for columns headers

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -21,15 +21,19 @@ function Table <T extends Record<string, any>>({ columns = [], rows = [] }: Tabl
   const [pagesNumber, setPagesNumber] = useState<number>(1);
   const [restOfEntries, setRestOfEntries] = useState<number>(0);
   const [displayedSample, setDisplayedSample] = useState<T[]>([]);
+  const [editingColumn, setEditingColumn] = useState<keyof T | null>(null);
+  const [searchQueries, setSearchQueries] = useState<{ [key in keyof T]: T[key] }[]>([]);
 
   const ascFilteringButtonRef = useRef<HTMLButtonElement>(null);
   const descFilteringButtonRef = useRef<HTMLButtonElement>(null);
 
-  const handleFilter = (e: React.MouseEvent<HTMLButtonElement>, property: keyof T, type: 'string' | 'number' | 'date' | 'boolean', filter: 'asc' | 'desc') => {
+  const columnNameRef = useRef<HTMLDivElement>(null);
+
+  const handleSort = (e: React.MouseEvent<HTMLButtonElement>, property: keyof T, type: 'string' | 'number' | 'date' | 'boolean', sort: 'asc' | 'desc') => {
     e.preventDefault();
 
     const arrayOfPropertyValues = rows.map((row) => row[property]);
-    if (filter === 'asc') {
+    if (sort === 'asc') {
       switch (type) {
         case 'string':
           arrayOfPropertyValues.sort((a, b) => a.localeCompare(b));
@@ -50,7 +54,7 @@ function Table <T extends Record<string, any>>({ columns = [], rows = [] }: Tabl
           arrayOfPropertyValues.sort((a, b) => a - b);
           break;
       }
-    } else if (filter === 'desc') {
+    } else if (sort === 'desc') {
       switch (type) {
         case 'string':
           arrayOfPropertyValues.sort((a, b) => b.localeCompare(a));
@@ -80,6 +84,14 @@ function Table <T extends Record<string, any>>({ columns = [], rows = [] }: Tabl
 
   const handleDisplayedEntriesChange = (e: ChangeEvent<HTMLSelectElement>) => {
     setSampleLength(parseInt(e.target.value));
+  }
+
+  const handleHeaderClick = (property: keyof T) => setEditingColumn(property);
+
+  const handleInputBlur = () => setEditingColumn(null);
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>, property: keyof T) => {
+    setSearchQueries(prev => ({ ...prev, [property]: e.target.value }));
   }
 
   useEffect(() => {
@@ -129,31 +141,53 @@ function Table <T extends Record<string, any>>({ columns = [], rows = [] }: Tabl
                 className='px-[10px] py-[5px] border-2 align-top'
               >
                 <div className='flex items-center justify-between'>
-                  <p className='mr-2'>{key.displayName}</p>
-                  <div className='flex flex-col gap-1'>
-                    <button type='button' ref={ascFilteringButtonRef} onClick={(e) => handleFilter(e, key.property, key.type, 'asc')} className='cursor-pointer'>
-                      <svg xmlns="http://www.w3.org/2000/svg" height="10px" viewBox="0 -960 960 960" width="10px" fill="#e8eaed">
-                        <path d="M152-160q-23 0-35-20.5t1-40.5l328-525q12-19 34-19t34 19l328 525q13 20 1 40.5T808-160H152Z"/>
-                      </svg>
-                    </button>
-                    <button type='button' ref={descFilteringButtonRef} onClick={(e) => handleFilter(e, key.property, key.type, 'desc')} className='cursor-pointer'>
-                      <svg className='rotate-180' xmlns="http://www.w3.org/2000/svg" height="10px" viewBox="0 -960 960 960" width="10px" fill="#e8eaed">
-                        <path d="M152-160q-23 0-35-20.5t1-40.5l328-525q12-19 34-19t34 19l328 525q13 20 1 40.5T808-160H152Z"/>
-                      </svg>
-                    </button>
-                  </div>
+                  {editingColumn === key.property ? (
+                    <div className='flex flex-col h-[55px] gap-1'>
+                      <label htmlFor={`search-${String(key.property)}`} className='pr-2.5'>{key.displayName} :</label>
+                      <input
+                        type="text"
+                        className='border-1 border-black rounded-[5px] w-[100%]'
+                        onChange={(e) => handleSearch(e, key.property)}
+                        onBlur={handleInputBlur}
+                        autoFocus
+                        name={`search-${String(key.property)}`}
+                      />
+                    </div>
+                  ) : (
+                    <div className='flex items-center w-[100%] h-[55px]'>
+                      <p
+                        ref={columnNameRef}
+                        className='mr-2 flex-1 cursor-pointer'
+                        onClick={() => handleHeaderClick(key.property) }                     
+                      >
+                        {key.displayName}
+                      </p>
+                      <div className='flex flex-col gap-1'>
+                        <button type='button' ref={ascFilteringButtonRef} onClick={(e) => handleSort(e, key.property, key.type, 'asc')} className='cursor-pointer'>
+                          <svg xmlns="http://www.w3.org/2000/svg" height="10px" viewBox="0 -960 960 960" width="10px" fill="#175729">
+                            <path d="M152-160q-23 0-35-20.5t1-40.5l328-525q12-19 34-19t34 19l328 525q13 20 1 40.5T808-160H152Z"/>
+                          </svg>
+                        </button>
+                        <button type='button' ref={descFilteringButtonRef} onClick={(e) => handleSort(e, key.property, key.type, 'desc')} className='cursor-pointer'>
+                          <svg className='rotate-180' xmlns="http://www.w3.org/2000/svg" height="10px" viewBox="0 -960 960 960" width="10px" fill="#175729">
+                            <path d="M152-160q-23 0-35-20.5t1-40.5l328-525q12-19 34-19t34 19l328 525q13 20 1 40.5T808-160H152Z"/>
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                  )} 
                 </div>
               </th>
             ))}
           </tr>
         </thead>
-        <tbody>
+        <tbody className='pb-2.5'>
           {displayedSample.map((row: T, rowIndex: number): ReactNode => (
             <tr key={rowIndex}>
               {columns.map((column, colIndex) => (
-                <td
+                <td title={`id: ${row.id}`}
                   key={colIndex}
-                  className='px-[10px] truncate'
+                  className='px-[15px] truncate'
                   style={{ width: `${100 / columns.length}%` }}
                 >
                   {row[column.property] as ReactNode}
