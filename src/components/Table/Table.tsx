171f6e7dd28bea4ce8a42da2f86bc51b5bc34e2d
Stéphane Bazzé <zarakeye@users.noreply.github.com>
2025-03-26 10:44:45 +0100

    new test css

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -439,8 +439,8 @@ function Table <T extends Record<string, any>>({
   )
 
   return (
-    <div className={`my-5 `}>
-      <div className='flex flex-col lg:flex-row lg:justify-between lg:items-center my-5 gap-y-3.5'>
+    <div className={`my-5`}>
+      <div className='flex flex-col lg:flex-row justify-between items-center my-5 gap-y-3.5'>
         <div>
           <label htmlFor="rangeLength" className='sr-only'>Displayed entries</label>
           <Dropdown>
